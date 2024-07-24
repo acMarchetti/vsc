1057b364e5c2959748594ce34cb7bb18c3e5774a
acMarchetti <andreacosmamarchetti@yahoo.com>
2024-07-24 09:11:37 +0200

    update 24/07 9:11

--- corsodev/loop.js
+++ corsodev/loop.js
@@ -1,23 +1,24 @@
 /*for (let i = 0; i >= -10; i--) {
-    console.log ("and", i, "ah ah ah");
-}
-console.log ("Count done");
+  console.log(i);
+} 
+console.log ("Count done");*/
 
-let j = 0
-while (j <= 10){
-    console.log (j);
-    j++;
-};*/
+let j = 0;
+while (j <= 10) {
+  console.log(j);
+  j++;
+}
+console.log(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
 
 /*senza suggerimento l'avrei fatto così
 for (let i=0; i <=20; i = i +2) {
     console.log (i)
 }
-console.log ("Done") */
+console.log ("Done") 
 
 for (let i = 0; i <=20; i++){
     if(i % 2 == 0){
         console.log(i);
     }
 };
-console.log ("Done");
+console.log ("Done");*/
